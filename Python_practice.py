fe02fc5bd7f77afbf25dc436a71de50d417ae8bf
Xin Lan <104236794+Orangexinlan@users.noreply.github.com>
2022-05-22 14:17:05 -0400

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,23 @@
+print("Hello World")
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+    for county in counties:
+     print (county)
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict:
+     print(county)
+for county in counties_dict.keys():
+    print(county)
+for voters in counties_dict.values():
+    print(voters)
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+for county_dict in voting_data:
+    print(county_dict)
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
